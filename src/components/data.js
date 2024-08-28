421f3529b69fb81e12922c2450fb04b819bb3fca
AYOUB134 <113133550+AYOUB134@users.noreply.github.com>
2024-08-28 05:26:49 -0700

    Update data.js

--- src/components/data.js
+++ src/components/data.js
@@ -33,45 +33,84 @@ export const questionsRound1 = [
     { id: 30, question: 'Each surface of a lens is a part of the', options: ['Sphere', 'Circle', 'Hexagon', 'Cube'], answer: 'Sphere' },
 ];
 
+// export const questionsRound2 = [
+//     // Questions 1-10
+//     { id: 1, question: 'The occipital lobe helps to process', options: ['Auditory information', 'Visual information', 'Language', 'Motor movements'], answer: 'Visual information' },
+//     { id: 2, question: 'It lies inside cerebrum and controls sensory functions.', options: ['Thalamus', 'Pons', 'Hypothalamus', 'Cerebellum'], answer: 'Thalamus' },
+//     { id: 3, question: 'The waves in which particle of the medium vibrate parallel to the direction of waves are called', options: ['Electromagnetic waves', 'Both b and c', 'Longitudinal waves', 'Transverse waves'], answer: 'Longitudinal waves' },
+//     { id: 4, question: 'The waves in which particles of the medium vibrate perpendicular to the direction of propagation of waves are called:', options: ['Electromagnetic waves', 'Longitudinal waves', 'Transverse waves', 'None of these'], answer: 'Transverse waves' },
+//     { id: 5, question: 'Which one of them is not a base?', options: ['Adenine', 'Guanine', 'Thalamine', 'Thymine'], answer: 'Thalamine' },
+//     { id: 6, question: 'Which of the following disease can be treated by the use of vaccine', options: ['Diabetes', 'Measles', 'AIDS', 'Cancer'], answer: 'Measles' },
+//     { id: 7, question: 'The process by which electrons are emitted by a hot metal surface is known as:', options: ['Conduction', 'Thermionic emission', 'Boiling', 'Evaporation'], answer: 'Thermionic emission' },
+//     { id: 8, question: 'The classical method used to check the quantity of a sample is', options: ['Titimetry', 'Gravimetry', 'Flame test', 'Chromatography'], answer: 'Gravimetry' },
+//     { id: 9, question: 'Which one of them is used for checking urine for diabetes', options: ['Spectrophotometer', 'Colorimeter', 'Microscope', 'Refractometer'], answer: 'Refractometer' },
+//     { id: 10, question: 'Hydrogen gas can be tested by:', options: ['Inflammation', 'Explosion', 'Color change', 'None of these'], answer: 'Explosion' },
+
+//     // Questions 11-20
+//     { id: 11, question: 'The positive ion of which of the following is used in medicines?', options: ['Potassium', 'Calcium', 'Sodium', 'Lithium'], answer: 'Lithium' },
+//     { id: 12, question: 'The general name given to the group of chemical reactions in which energy is released is:', options: ['Endothermic', 'Exothermic', 'Photosynthesis', 'Respiration'], answer: 'Exothermic' },
+//     { id: 13, question: 'Which of the following has the highest frequency?', options: ['X-rays', 'Radio waves', 'Microwaves', 'Infrared'], answer: 'X-rays' },
+//     { id: 14, question: 'The glassware used to measure exact volume of liquid in laboratories is', options: ['Beaker', 'Graduated cylinder', 'Flask', 'Pipette'], answer: 'Graduated cylinder' },
+//     { id: 15, question: 'How many fundamental forces exist in nature?', options: ['Three', 'Four', 'Five', 'Six'], answer: 'Four' },
+//     { id: 16, question: 'The universal solvent is:', options: ['Water', 'Alcohol', 'Ethanol', 'Acetone'], answer: 'Water' },
+//     { id: 17, question: 'An acid that forms two hydrogen ions when dissolved in water is known as:', options: ['Mono-protic acid', 'Di-protic acid', 'Tri-protic acid', 'Tetra-protic acid'], answer: 'Di-protic acid' },
+//     { id: 18, question: 'What is the chemical symbol for silver?', options: ['Ag', 'Au', 'Si', 'S'], answer: 'Ag' },
+//     { id: 19, question: 'Which of the following materials is the best conductor of electricity?', options: ['Copper', 'Gold', 'Aluminum', 'Iron'], answer: 'Gold' },
+//     { id: 20, question: 'The unit of electric resistance is:', options: ['Ohm', 'Ampere', 'Volt', 'Watt'], answer: 'Ohm' },
+
+//     // Questions 21-30
+//     { id: 21, question: 'Which device is used to convert electrical energy into mechanical energy?', options: ['Generator', 'Motor', 'Battery', 'Transformer'], answer: 'Motor' },
+//     { id: 22, question: 'The energy required to remove one mole of electrons from one mole of an atom in gaseous state is:', options: ['Ionization energy', 'Electronegativity', 'Electron affinity', 'Bond energy'], answer: 'Ionization energy' },
+//     { id: 23, question: 'Which one is a characteristic of an acidic solution?', options: ['It turns blue litmus paper red', 'It turns red litmus paper blue', 'It has a bitter taste', 'It is slippery'], answer: 'It turns blue litmus paper red' },
+//     { id: 24, question: 'Which of the following is a noble gas?', options: ['Neon', 'Nitrogen', 'Oxygen', 'Hydrogen'], answer: 'Neon' },
+//     { id: 25, question: 'What is the pH of a neutral solution?', options: ['7', '0', '14', '10'], answer: '7' },
+//     { id: 26, question: 'The point at which a substance changes from solid to liquid is known as:', options: ['Boiling point', 'Melting point', 'Freezing point', 'Sublimation point'], answer: 'Melting point' },
+   
+    
+//     { id: 27, question: 'The speed of sound in air is highest at:', options: ['High temperatures', 'Low temperatures', 'High altitudes', 'Low altitudes'], answer: 'High temperatures' },
+
+//     { id: 28, question: 'The speed of sound in air is highest at:', options: ['High temperatures', 'Low temperatures', 'High altitudes', 'Low altitudes'], answer: 'High temperatures' },
+//     { id: 29, question: 'Which of the following is not a type of chemical bond?', options: ['Covalent', 'Ionic', 'Hydrogen', 'Magnetic'], answer: 'Magnetic' },
+//     { id: 30, question: 'The process of converting a liquid to a gas is called:', options: ['Evaporation', 'Condensation', 'Sublimation', 'Freezing'], answer: 'Evaporation' },
+// ];
+
+
 export const questionsRound2 = [
     // Questions 1-10
     { id: 1, question: 'The occipital lobe helps to process', options: ['Auditory information', 'Visual information', 'Language', 'Motor movements'], answer: 'Visual information' },
     { id: 2, question: 'It lies inside cerebrum and controls sensory functions.', options: ['Thalamus', 'Pons', 'Hypothalamus', 'Cerebellum'], answer: 'Thalamus' },
-    { id: 3, question: 'The waves in which particle of the medium vibrate parallel to the direction of waves are called', options: ['Electromagnetic waves', 'Both b and c', 'Longitudinal waves', 'Transverse waves'], answer: 'Longitudinal waves' },
-    { id: 4, question: 'The waves in which particles of the medium vibrate perpendicular to the direction of propagation of waves are called:', options: ['Electromagnetic waves', 'Longitudinal waves', 'Transverse waves', 'None of these'], answer: 'Transverse waves' },
-    { id: 5, question: 'Which one of them is not a base?', options: ['Adenine', 'Guanine', 'Thalamine', 'Thymine'], answer: 'Thalamine' },
-    { id: 6, question: 'Which of the following disease can be treated by the use of vaccine', options: ['Diabetes', 'Measles', 'AIDS', 'Cancer'], answer: 'Measles' },
-    { id: 7, question: 'The process by which electrons are emitted by a hot metal surface is known as:', options: ['Conduction', 'Thermionic emission', 'Boiling', 'Evaporation'], answer: 'Thermionic emission' },
-    { id: 8, question: 'The classical method used to check the quantity of a sample is', options: ['Titimetry', 'Gravimetry', 'Flame test', 'Chromatography'], answer: 'Gravimetry' },
-    { id: 9, question: 'Which one of them is used for checking urine for diabetes', options: ['Spectrophotometer', 'Colorimeter', 'Microscope', 'Refractometer'], answer: 'Refractometer' },
-    { id: 10, question: 'Hydrogen gas can be tested by:', options: ['Inflammation', 'Explosion', 'Color change', 'None of these'], answer: 'Explosion' },
+    { id: 3, question: 'This branch of physics deals with heat and temperature and their relation to energy and work:', options: ['Mechanics', 'Thermodynamics', 'Astrophysics', 'Geophysics'], answer: 'Thermodynamics' },
+    { id: 4, question: 'The force required to move a body along a circular path is called:', options: ['Gravitational force', 'Centripetal force', 'Centrifugal force', 'Torque'], answer: 'Centripetal force' },
+    { id: 5, question: 'The waves in which particle of the medium vibrate parallel to the direction of waves are called', options: ['Electromagnetic waves', 'Both b and c', 'Longitudinal waves', 'Transverse waves'], answer: 'Longitudinal waves' },
+    { id: 6, question: 'The waves in which particles of the medium vibrate perpendicular to the direction of propagation of waves are called:', options: ['Electromagnetic waves', 'Longitudinal waves', 'Transverse waves', 'None of these'], answer: 'Transverse waves' },
+    { id: 7, question: 'Membrane bounded green pigment-containing bodies present in the cells are called as:', options: ['Plastids', 'Chloroplasts', 'Chromoplasts', 'Leucoplasts'], answer: 'Chloroplasts' },
+    { id: 8, question: 'Any foreign object that gains entry inside the cell is immediately engulfed by:', options: ['Lysosome', 'Ribosome', 'Peroxisome', 'Glyoxisome'], answer: 'Lysosome' },
+    { id: 9, question: 'Which one of them is not a base?', options: ['Adenine', 'Guanine', 'Thalamine', 'Thymine'], answer: 'Thalamine' },
+    { id: 10, question: 'Which of the following disease can be treated by the use of vaccine', options: ['Diabetes', 'Measles', 'AIDS', 'Cancer'], answer: 'Measles' },
 
     // Questions 11-20
-    { id: 11, question: 'The positive ion of which of the following is used in medicines?', options: ['Potassium', 'Calcium', 'Sodium', 'Lithium'], answer: 'Lithium' },
-    { id: 12, question: 'The general name given to the group of chemical reactions in which energy is released is:', options: ['Endothermic', 'Exothermic', 'Photosynthesis', 'Respiration'], answer: 'Exothermic' },
-    { id: 13, question: 'Which of the following has the highest frequency?', options: ['X-rays', 'Radio waves', 'Microwaves', 'Infrared'], answer: 'X-rays' },
-    { id: 14, question: 'The glassware used to measure exact volume of liquid in laboratories is', options: ['Beaker', 'Graduated cylinder', 'Flask', 'Pipette'], answer: 'Graduated cylinder' },
-    { id: 15, question: 'How many fundamental forces exist in nature?', options: ['Three', 'Four', 'Five', 'Six'], answer: 'Four' },
-    { id: 16, question: 'The universal solvent is:', options: ['Water', 'Alcohol', 'Ethanol', 'Acetone'], answer: 'Water' },
-    { id: 17, question: 'An acid that forms two hydrogen ions when dissolved in water is known as:', options: ['Mono-protic acid', 'Di-protic acid', 'Tri-protic acid', 'Tetra-protic acid'], answer: 'Di-protic acid' },
-    { id: 18, question: 'What is the chemical symbol for silver?', options: ['Ag', 'Au', 'Si', 'S'], answer: 'Ag' },
-    { id: 19, question: 'Which of the following materials is the best conductor of electricity?', options: ['Copper', 'Gold', 'Aluminum', 'Iron'], answer: 'Gold' },
-    { id: 20, question: 'The unit of electric resistance is:', options: ['Ohm', 'Ampere', 'Volt', 'Watt'], answer: 'Ohm' },
+    { id: 11, question: 'The length of spring increases as the force increases. This increase in length of spring is known as:', options: ['Extension', 'Stretch', 'Elasticity', 'Expansion'], answer: 'Extension' },
+    { id: 12, question: 'During which process a gas becomes a liquid', options: ['Melting', 'Freezing', 'Condensing', 'Boiling'], answer: 'Condensing' },
+    { id: 13, question: 'The process by which electrons are emitted by a hot metal surface is known as:', options: ['Conduction', 'Thermionic emission', 'Boiling', 'Evaporation'], answer: 'Thermionic emission' },
+    { id: 14, question: 'The classical method used to check the quantity of a sample is', options: ['Titimetry', 'Gravimetry', 'Flame test', 'Chromatography'], answer: 'Gravimetry' },
+    { id: 15, question: 'Aerobic bacteria are:', options: ['Campylobacter', 'E. coli', 'Pseudomonas', 'None of these'], answer: 'Pseudomonas' },
+    { id: 16, question: 'The causative agent of African sleeping sickness is:', options: ['Tsetse fly', 'Mosquito', 'Trypanosoma', 'Trichonympha'], answer: 'Trypanosoma' },
+    { id: 17, question: 'It’s the main gas responsible for asthma attacks', options: ['Carbon dioxide', 'Sulphur dioxide', 'Nitrogen dioxide', 'Methane'], answer: 'Sulphur dioxide' },
+    { id: 18, question: 'The strongest intermolecular forces are present in', options: ['Solid', 'Gas', 'Plasma', 'Liquid'], answer: 'Solid' },
+    { id: 19, question: 'Alkaline earth metals form', options: ['Monovalent cation', 'Monovalent anion', 'Divalent cation', 'Divalent anion'], answer: 'Divalent cation' },
+    { id: 20, question: 'It is a technique used in analytical chemistry for separation of volatile compounds', options: ['Visible spectroscopy', 'Infra-red spectroscopy', 'Liquid chromatography', 'Gas chromatography'], answer: 'Gas chromatography' },
 
     // Questions 21-30
-    { id: 21, question: 'Which device is used to convert electrical energy into mechanical energy?', options: ['Generator', 'Motor', 'Battery', 'Transformer'], answer: 'Motor' },
-    { id: 22, question: 'The energy required to remove one mole of electrons from one mole of an atom in gaseous state is:', options: ['Ionization energy', 'Electronegativity', 'Electron affinity', 'Bond energy'], answer: 'Ionization energy' },
-    { id: 23, question: 'Which one is a characteristic of an acidic solution?', options: ['It turns blue litmus paper red', 'It turns red litmus paper blue', 'It has a bitter taste', 'It is slippery'], answer: 'It turns blue litmus paper red' },
-    { id: 24, question: 'Which of the following is a noble gas?', options: ['Neon', 'Nitrogen', 'Oxygen', 'Hydrogen'], answer: 'Neon' },
-    { id: 25, question: 'What is the pH of a neutral solution?', options: ['7', '0', '14', '10'], answer: '7' },
-    { id: 26, question: 'The point at which a substance changes from solid to liquid is known as:', options: ['Boiling point', 'Melting point', 'Freezing point', 'Sublimation point'], answer: 'Melting point' },
-   
-    
-    { id: 27, question: 'The speed of sound in air is highest at:', options: ['High temperatures', 'Low temperatures', 'High altitudes', 'Low altitudes'], answer: 'High temperatures' },
-
-    { id: 28, question: 'The speed of sound in air is highest at:', options: ['High temperatures', 'Low temperatures', 'High altitudes', 'Low altitudes'], answer: 'High temperatures' },
-    { id: 29, question: 'Which of the following is not a type of chemical bond?', options: ['Covalent', 'Ionic', 'Hydrogen', 'Magnetic'], answer: 'Magnetic' },
-    { id: 30, question: 'The process of converting a liquid to a gas is called:', options: ['Evaporation', 'Condensation', 'Sublimation', 'Freezing'], answer: 'Evaporation' },
+    { id: 21, question: 'The amount of time that a compound spends in the stationary phase is', options: ['Retention', 'Combination', 'Separation', 'Pre-caution'], answer: 'Retention' },
+    { id: 22, question: 'Which of the following is not a dimensionless quantity?', options: ['Reynolds number', 'Mach number', 'Specific gravity', 'Pressure'], answer: 'Pressure' },
+    { id: 23, question: 'When a solid body is totally immersed in a liquid, it loses weight equal to the weight of the liquid displaced. This principle is known as:', options: ['Bernoulli\'s principle', 'Pascal\'s principle', 'Archimedes\' principle', 'Hooke\'s law'], answer: 'Archimedes\' principle' },
+    { id: 24, question: '___________ reactions are those reactions in which heat energy is absorbed or added to the system.', options: ['Exothermic', 'Endothermic', 'Addition', 'Decomposition'], answer: 'Endothermic' },
+    { id: 25, question: 'The material which sometimes acts as a conductor or a non-conductor is known as:', options: ['Silicon', 'Semiconductor', 'Cathode', 'Conditional conductor'], answer: 'Semiconductor' },
+    { id: 26, question: 'With increasing atomic size the ionization energy', options: ['Increase', 'Decrease', 'Fluctuates', 'Remains constant'], answer: 'Decrease' },
+    { id: 27, question: 'Biological name of cat is:', options: ['Felis catus', 'Canis lupus', 'Allium cepa', 'Azadirachta indica'], answer: 'Felis catus' },
+    { id: 28, question: 'What is the term for the body\'s ability to maintain a stable internal environment despite external changes?', options: ['Equilibrium', 'Stability', 'Homeostasis', 'Balance'], answer: 'Homeostasis' },
+    { id: 29, question: 'Which neurotransmitter is associated with sleep, mood, and appetite regulation?', options: ['Dopamine', 'Serotonin', 'Acetylcholine', 'Endorphins'], answer: 'Serotonin' },
+    { id: 30, question: 'The retina has numerous ________ cells that help change the light rays into electrical impulses', options: ['Nerve', 'Photoreceptor nerve', 'Neurons', 'Muscle'], answer: 'Photoreceptor nerve' },
 ];
 
 export const questionsRound3 = [
